d2a6e7a4eb93fb169fc31b7ac43de9c47c7f2c19
Jake Varley <jakevarley@gmail.com>
2017-08-31 10:52:11 -0400

    formatting setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@ from distutils.core import setup
 
 setup(name="curvox",
       version='1.0',
-      description='Python library for creating and initializing point clouds from voxel grids',
+      description=
+      """
+      Python library with utilities for converting pointclouds, meshes,
+      and voxel grids between ros messages, files matrices, objects
+      """,
       author='David Watkins',
       author_email='davidwatkins@cs.columbia.edu',
       url='https://github.com/CURG/curvox',
       packages=['curvox'],
-      dependency_links = ['https://github.com/CURG/binvox-rw-py/tarball/master#egg=binvox-rw-1.0'],
+      dependency_links=['https://github.com/CURG/binvox-rw-py/tarball/master#egg=binvox-rw-1.0'],
       install_requires=[
-        "numpy >= 1.8",
-        "binvox-rw >= 1.0",
-        "plyfile >= 0.4"
-      ]
-    )
+          "numpy >= 1.8",
+          "binvox-rw >= 1.0",
+          "plyfile >= 0.4"
+      ])
